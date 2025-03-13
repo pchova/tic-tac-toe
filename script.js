9ae1c16a0a2061626ba24299a7095c9203d93d51
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2025-03-12 18:34:25 -0700

    added restartGame() which restarts game after board is filled

--- script.js
+++ script.js
@@ -64,7 +64,6 @@ function Cell() {
 const DisplayController = (function() {
     const player1 = CreatePlayer("pchova");
     const player2 = CreatePlayer("anhellll");
-
     const players = [
         {
             name: player1.displayName,
@@ -81,29 +80,32 @@ const DisplayController = (function() {
     console.log(`Its ${players[0].name}'s turn first!`);
 
     /* switchPlayerTurn() switches between the two players
-    ** getActivePlayer() returns whose turn it is 
-    */
+    ** getActivePlayer() returns whose turn it is */
     let activePlayer = players[0];
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
     const getActivePlayer = () => activePlayer;
 
+    /* Variable count is set to 0, setter/getter functions are accessed within
+    ** playRound() to keep track of when the board is filled */
     let count = 0;
     const getCount = () => count;
-    const setCount = () => ++count;
+    const setCount = () => count++;
 
+    /* PlayRound() adds token to a cell on the board
+    ** Plays more rounds till count = 8, displays winner/tie
+    ** Option to restart game  */
     const playRound = (row, column) => {
-        console.log(getCount());
-
+        
         if(Gameboard.addMove(row, column, activePlayer.token) === false) {
             return `${activePlayer.name}, spot (${row},${column}) is taken, please try again.`;
         } else if (getCount() === 8) {
-            //add function determineWinner() to determine if someone won!!!
-            //add if/else if/else statements to go through each situation
-            
+            /* add function determineWinner() to determine if someone won,
+            ** and add if/else if/else statements to go through each situation */
             console.log("nobody won. Please play again!");
-            return printBoard();
+            printBoard();
+            return restartGame();
         } else {
             Gameboard.addMove(row, column, activePlayer.token);
             switchPlayerTurn();
@@ -114,6 +116,7 @@ const DisplayController = (function() {
         console.log("count: " + getCount());
     };
 
+    /* Print's current board state and active player's turn */
     const printNextRound = () => {
         const board = Gameboard.getBoard();
         const displayBoard = board.map(row => row.map(cell => cell.getValue()));
@@ -122,11 +125,28 @@ const DisplayController = (function() {
         console.log(`It's ${activePlayer.name}'s turn!`);
     }
 
+    /* Restarts game by setting count to 0 and clearing board array to be empty */
+    const restartGame = () => {
+        switchPlayerTurn();
+        console.log(`Restarting Game....it's ${activePlayer.name}'s turn!`);
+
+        count = 0;
+
+        const newBoard = Gameboard.getBoard();
+        for(let i = 0; i < newBoard.length; i++) {
+            for(let j = 0; j < newBoard[i].length; j++) {
+                newBoard[i][j].setValue("");
+            }
+        }
+
+        printBoard();
+    }
+
     const printBoard = () => {
         const board = Gameboard.getBoard();
         const displayBoard = board.map(row => row.map(cell => cell.getValue()));
         console.log(displayBoard);
-    }
+    } 
 
-    return {getActivePlayer, playRound, printNextRound};
+    return {getActivePlayer, playRound, printBoard, getCount};
 })();
